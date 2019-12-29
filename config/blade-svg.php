50aa97a07f993a864ac11846b7fee40df4520a22
Martin Betz <martinbetz@Mini-2018.fritz.box>
2019-12-29 21:53:34 +0100

    Add: Basic SVGs

---
+++ config/blade-svg.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'svg_path' => 'resources/svg',
+    'spritesheet_path' => 'resources/svg/spritesheet.svg',
+    'spritesheet_url' => '',
+    'inline' => true,
+    'class' => 'stroke-current inline-block w-4 h-4',
+];
