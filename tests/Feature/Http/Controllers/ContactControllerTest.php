abeffacad8fa72e2244c5927fc741a528398b637
Martin Betz <martinbetz@Mini-2018.fritz.box>
2019-12-31 00:43:39 +0100

    Add: Test user_can_see_list_of_notes_for_contact

---
+++ tests/Feature/Http/Controllers/ContactControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Contact;
+use App\Note;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ContactControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function user_can_see_list_of_notes_for_contact()
+    {
+        $this->withoutExceptionHandling();
+        $contact = factory(Contact::class)->create();
+        $note = factory(Note::class)->create();
+
+        $response = $this->get(route('contact.show', $contact));
+        $response->assertSee(e($note->body));
+    }
+}
