5c3a3935482b661eb6c6136a68cdb7b87a25b45b
Martin Betz <martinbetz@Mini-2018.fritz.box>
2019-12-31 14:46:56 +0100

    Add: type_description accessor

--- app/Note.php
+++ app/Note.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Enums\NoteType;
 use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
@@ -9,4 +10,9 @@ class Note extends Model
     protected $fillable = [
         'contact_id', 'type', 'body',
     ];
+
+    public function getTypeDescriptionAttribute()
+    {
+        return NoteType::getDescription($this->type);
+    }
 }
