6a39fa30c4133d6403d40ecbe4985f3b3aaf6002
Martin Betz <martinbetz@Mini-2018.fritz.box>
2020-01-05 12:39:42 +0100

    Refactor: Move note and contact into auth middleware route group

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 Auth::routes();
 
+Route::middleware('auth')->group(function () {
+    Route::resource('contact', 'ContactController');
+    Route::resource('note', 'NoteController');
+});
+
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('contact', 'ContactController');
-Route::resource('note', 'NoteController');
