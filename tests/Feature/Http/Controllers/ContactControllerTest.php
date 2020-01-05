2a54082e994a96d223b9557fed808291fc9520c4
Martin Betz <martinbetz@Mini-2018.fritz.box>
2020-01-05 12:41:40 +0100

    Fix: Add user factory to test user_can_see_list_of_notes_for_contact

--- tests/Feature/Http/Controllers/ContactControllerTest.php
+++ tests/Feature/Http/Controllers/ContactControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Http\Controllers;
 use App\Contact;
 use App\Note;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
 use Tests\TestCase;
 
 class ContactControllerTest extends TestCase
@@ -15,10 +16,11 @@ class ContactControllerTest extends TestCase
     public function user_can_see_list_of_notes_for_contact()
     {
         $this->withoutExceptionHandling();
+        $user = factory(User::class)->create();
         $contact = factory(Contact::class)->create();
         $note = factory(Note::class)->create();
 
-        $response = $this->get(route('contact.show', $contact));
+        $response = $this->actingAs($user)->get(route('contact.show', $contact));
         $response->assertSee(e($note->body));
     }
 
